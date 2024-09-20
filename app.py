61345b8acfbefbf1b739b253862485c3da1a1b34
NdL <ndelamberterie@the-swiss-trust.com>
2024-09-20 12:17:50 +0200

    3d commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from PIL import Image, ImageOps
 import boto3
+import tensorflow as tf
 
 aws_access_key_id = st.secrets["AWS_ACCESS_KEY_ID"]
 aws_secret_access_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
