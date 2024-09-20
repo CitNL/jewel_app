0b861ad375f4bfdcf3cdebe6cea4bc6f47739a60
NdL <ndelamberterie@the-swiss-trust.com>
2024-09-20 09:40:44 +0200

    2d commit

--- app.py
+++ app.py
@@ -23,13 +23,34 @@ def load_trained_model_from_S3():
 
 model = load_trained_model_from_S3()
 
+
+@st.cache(allow_output_mutation=True)
+def load_file_from_S3():
+
+    s3_client = boto3.client('s3')
+
+    bucket_name = 'ndl-sandbox'
+    jewel_model = 'jewel-classifier/categories.txt'
+
+    try:
+        file = s3_client.get_object(Bucket=bucket_name, Key=jewel_model)
+
+    except Exception as e:
+        print("Error accessing S3:", e)
+        return None
+    return file['Body'].read().decode('utf-8')
+
+
+cat_file=load_file_from_S3()
+
 # Create a mapping for model predictions
-with open('categories.txt', 'r') as f:
+with open(cat_file, 'r') as f:
     categories = [line.strip() for line in f.readlines()]
 
 # Streamlit App interface
 st.title("Jewel Classification App")
 st.write("Upload an image to classify the jewel.")
+st.write(categories[0])
 
 # File uploader for image input
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
