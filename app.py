e7db46efff65ecee66815559369a7f71158911e3
NdL <ndelamberterie@the-swiss-trust.com>
2024-09-20 14:06:22 +0200

    3d commit

--- app.py
+++ app.py
@@ -9,30 +9,28 @@ from tensorflow.keras.models import load_model
 aws_access_key_id = st.secrets["AWS_ACCESS_KEY_ID"]
 aws_secret_access_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
 
-#@st.cache(allow_output_mutation=True)
+@st.cache_ressource()
 def load_trained_model_from_S3():
 
     s3_client = boto3.client('s3',
                              aws_access_key_id=aws_access_key_id,
                              aws_secret_access_key=aws_secret_access_key)
-
+    
     bucket_name = 'ndl-sandbox'
     jewel_model = 'jewel-classifier/jewel_classifier_resnet.h5'
     # Temporary local path
     local_path = 'jewel_model.h5'  # Adjust based on your environment #'/tmp/jewel_model.h5'
-
-    #response = s3_client.get_object(Bucket=bucket_name, Key=jewel_model)
-    s3_client.download_file(bucket_name, jewel_model, local_path)
-    st.write('File found and OKKKKKK!!!!!')
-    model = load_model(local_path)
-    st.write('model loaded OKKKKKK!!!!!')
-
     try:
-        1==1
+        #response = s3_client.get_object(Bucket=bucket_name, Key=jewel_model)
+        s3_client.download_file(bucket_name, jewel_model, local_path)
+        st.write('File found and OKKKKKK!!!!!')
+        model = load_model(local_path)
+        st.write('model loaded OKKKKKK!!!!!')
 
     except Exception as e:
-        print("Error accessing S3:", e)
+        st.error(f"Error: {e}")
         return None
+    
     return model
 
 if st.button("Load Model"):
@@ -42,7 +40,7 @@ if st.button("Load Model"):
 
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache_ressource()
 def read_file_from_s3(bucket_name, file_key):
 
     s3_client = boto3.client('s3',
@@ -61,33 +59,28 @@ def read_file_from_s3(bucket_name, file_key):
     print('file loaded')
     return content
 
-# Streamlit App interface
-st.title("Jewel Classification App")
-st.write("Upload an image to classify the jewel.")
 
-if st.button("Load File"):
+@st.cache_data()
+def load_categories():
         bucket_name = 'ndl-sandbox'
         file_key = 'jewel-classifier/categories.txt'
         try:
             content = read_file_from_s3(bucket_name, file_key)
-            if content is not None:
-                # Display the file content
-                st.text_area("File Content", content, height=400)
-            
+            if content is not None:           
                 # Split into categories if content exists
                 categories = content.split('\n')
-                st.write(categories)
+                st.write('Category labels uploaded')
             else:
-                st.error("Failed to load content from the file.")
+                st.error("Failed to load category labels")
 
         except Exception as e:
             st.error(f"Error: {e}")
 
+load_categories()
 
-    
-
-
-
+# Streamlit App interface
+st.title("Jewel Classification App")
+st.write("Upload an image to classify the jewel.")
 
 # File uploader for image input
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
@@ -96,4 +89,14 @@ if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
+
+# Preprocess the image
+    processed_image = preprocess_image(image, target_size=(224, 224))  # adjust target_size based on your model's input size
     
+    # Make predictions
+    predictions = model.predict(processed_image)
+    predicted_class = np.argmax(predictions[0])
+    
+    # Display the result
+    st.write(f"Prediction: {categories[predicted_class]}")
+    st.write(f"Confidence: {np.max(predictions[0]) * 100:.2f}%")
