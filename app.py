74dd34e2165949f1ba37c91de74c902455b62614
NdL <ndelamberterie@the-swiss-trust.com>
2024-09-20 11:20:43 +0200

    3d commit

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def read_file_from_s3(bucket_name, file_key):
 
     s3_client = boto3.client('s3',
                              aws_access_key_id=aws_access_key_id,
-                             aws_secret_access_key=aws_secret_access_key))
+                             aws_secret_access_key=aws_secret_access_key)
     try:
         response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
         st.write('File found and OKKKKKK!!!!!')
