f7289036c78cad0fb4f5aff4e4dfcdbdfa9b0ac1
NdL <ndelamberterie@the-swiss-trust.com>
2024-09-20 12:36:08 +0200

    3d commit

--- app.py
+++ app.py
@@ -22,9 +22,10 @@ def load_trained_model_from_S3():
     local_path = 'jewel_model.h5'  # Adjust based on your environment #'/tmp/jewel_model.h5'
 
     try:
-        s3.download_file(bucket_name, jewel_model, local_path)
+        response = s3_client.get_object(bucket_name, jewel_model)
+        #s3.download_file(bucket_name, jewel_model, local_path)
         st.write('File found and OKKKKKK!!!!!')
-        model = load_model(local_path)
+        #model = load_model(local_path)
         st.write('model loaded OKKKKKK!!!!!')
 
     except Exception as e:
